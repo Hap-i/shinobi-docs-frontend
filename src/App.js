c1641f327da138f9b17325e0ae40f6652e9bdecc
suvendu sekhar sahoo <suvendusekharsahoo5@gmail.com>
2022-06-06 21:49:55 +0530

    added sign up and login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AllDocument from "./pages/AllDocument";
 import TextEditor from "./pages/TextEditor";
+import SignUp from "./pages/Signup";
+import SignIn from "./pages/SignIn";
 function App() {
   return (
     <BrowserRouter>
@@ -12,6 +14,9 @@ function App() {
           element={<AllDocument />}
         ></Route>
         <Route path="/document/:id" element={<TextEditor />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Signup.js
@@ -0,0 +1,170 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="#">
+                Shinobi Docs
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const theme = createTheme();
+
+export default function SignUp() {
+    const navigate = useNavigate();
+    const toastOptions = {
+        position: "top-right",
+        autoClose: 3000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "light",
+    };
+    const validateData = (data) => {
+        var userData = {
+            firstname: data.get('firstName'),
+            lastname: data.get('lastName'),
+            email: data.get('email'),
+            password: data.get('password'),
+            confimpassword: data.get('confimPassword')
+        }
+
+        if (!userData.firstname) {
+            toast.error("First Name is mandatory", toastOptions)
+        } else if (!userData.lastname) {
+            toast.error("Last Name is mandatory", toastOptions)
+        } else if (!userData.email) {
+            toast.error("Email Name is mandatory", toastOptions)
+        } else if (!userData.password) {
+            toast.error("Password is mandatory", toastOptions)
+        } else if (!userData.confimpassword) {
+            toast.error("Confirm Password is mandatory", toastOptions)
+        } else if (userData.password !== userData.confimpassword) {
+            toast.error("Password and Confim Password isn't matching", toastOptions)
+        }
+
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        validateData(data)
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    name="firstName"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="First Name"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="lastName"
+                                    label="Last Name"
+                                    name="lastName"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="confimPassword"
+                                    label="Confirm Password"
+                                    type="password"
+                                    id="confimPassword"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <FormControlLabel
+                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                    label="I accept the terms and conditions."
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <Link href="#" variant="body2" onClick={navigate('/signin')}>
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 5 }} />
+            </Container>
+            <ToastContainer />
+        </ThemeProvider>
+    );
+}
