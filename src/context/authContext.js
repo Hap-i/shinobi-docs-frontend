1ce92f740b8760ff0c66d3bfa2d0876732bad493
suvendu sekhar sahoo <suvendusekharsahoo5@gmail.com>
2022-06-09 01:11:21 +0530

    added Auth Context

---
+++ src/context/authContext.js
@@ -0,0 +1,22 @@
+import React, { useContext, useState } from 'react'
+// create context
+const AuthContext = React.createContext()
+
+// make custom hook to call
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+// logic and wrap children
+export function AuthProvider({ children }) {
+    const [user, setuser] = useState();
+
+    function setUser(user) {
+        setuser(user);
+    }
+    return (
+        <AuthContext.Provider value={{ user, setUser }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
